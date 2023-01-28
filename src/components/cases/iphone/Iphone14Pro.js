a36376413bcc0d21aa80438be869c6ca37c413c3
Allen Mordukhaev <allenmor93@gmail.com>
2023-01-28 17:32:26 -0500

    head

--- src/components/cases/iphone/Iphone14Pro.js
+++ src/components/cases/iphone/Iphone14Pro.js
@@ -19,7 +19,7 @@ function Iphone14Pro() {
     <div>
       <NavBar />
       <CasesNav />
-      <h1>Iphone 11 cases</h1>
+      <h1>Iphone 14 Plus cases</h1>
       {cases.map((el, i) => {
         return <IphoneCard key={i} iphone={el}/>
       })}
