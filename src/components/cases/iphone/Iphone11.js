171aa27ca5c788b334a93312b904b5585cb999b7
Allen Mordukhaev <allenmor93@gmail.com>
2023-01-28 15:03:31 -0500

    json now

--- src/components/cases/iphone/Iphone11.js
+++ src/components/cases/iphone/Iphone11.js
@@ -1,30 +1,17 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { db } from '../../../firebase'
-import {collection, getDocs} from 'firebase/firestore'
+
 import NavBar from '../../NavBar'
 import CasesNav from '../../CasesNav'
-import Iphone11Card from './Iphone11Card'
+
 
 function Iphone11() {
-    const [cases, setCases] = useState([])
-    useEffect(()=>{
-      const casesCollectionRef = collection(db, 'iphone11')
-      const getCases = async() => {
-        const data = await getDocs(casesCollectionRef)
-        // setCases(data.docs.map((doc) => ({...doc.data()})))
-        console.log(data.docs.map((doc) => ({...doc.data()})));
-        setCases(data.docs.map((doc) => ({...doc.data()})))
-      }
-      getCases()
-    },[])
+
+
   return (
     <div>
       <NavBar />
       <CasesNav />
-      {cases.map((el, i) => {
-        return <Iphone11Card key={i} iphone={el}/>
-      })}
+
     </div>
   )
 }
