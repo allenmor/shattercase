9b012882cea9aaea67adf97e70e6684ba01148db
Allen Mordukhaev <allenmor93@gmail.com>
2023-03-16 16:49:03 -0400

    filter

--- src/components/cases/iphone/Iphone11.js
+++ src/components/cases/iphone/Iphone11.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../../NavBar";
 import CasesNav from "../../CasesNav";
 import IphoneCard from "./IphoneCard";
-import { useState, useEffect } from "react";
 
 function Iphone11() {
   const [cases, setCases] = useState([]);
+  const [selectedColor, setSelectedColor] = useState("");
+
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/allenmor/shattercase/main/db.json")
       .then((res) => res.json())
@@ -13,13 +14,54 @@ function Iphone11() {
         setCases(data[0].iphone11);
       });
   }, []);
+
+  const handleColorChange = (event) => {
+    setSelectedColor(event.target.value.toLowerCase());
+  };
+
+  const filteredCases = selectedColor === "all colors"
+    ? cases
+    : cases.filter((item) => item.item.toLowerCase().includes(selectedColor));
+
+
+  const allColors = [
+    "All colors",
+    "Black",
+    "White",
+    "Red",
+    "Blue",
+    "Green",
+    "Yellow",
+    "Purple",
+    "Pink",
+    'Teal',
+    'Transparent',
+    'Wallet',
+    'Design',
+    'Glitter',
+    'Kickstand',
+    'Clip',
+    'Shockproof',
+    'Magnetic',
+    'Ring',
+  ];
+
   return (
     <>
       <NavBar />
       <CasesNav />
-        <h1 className="phone-header">Iphone 11 cases</h1>
+      <div className="color-div">
+        <label htmlFor="color-select">Filter by color:</label>
+        <select id="color-select" onChange={handleColorChange}>
+          {allColors.map((color, i) => (
+            <option key={i} value={color.toLowerCase()}>
+              {color}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="phone-cases-div">
-        {cases.map((el, i) => {
+        {filteredCases.map((el, i) => {
           return <IphoneCard key={i} iphone={el} />;
         })}
       </div>
