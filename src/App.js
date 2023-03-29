3d1940555c0a4f1e13432efa5440e33d4c3f892a
Allen Mordukhaev <allenmor93@gmail.com>
2023-03-29 13:41:05 -0400

    business card added

--- src/App.js
+++ src/App.js
@@ -1,65 +1,15 @@
-// import Signup from "./components/Signup";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Routes, Route } from "react-router-dom";
-// import Dashboard from "./components/Dashboard";
-// import Login from "./components/Login";
-// import PrivateRoute from "./components/PrivateRoute";
-// import ForgotPassword from "./components/ForgotPassword";
-// import UpdateProfile from "./components/UpdateProfile";
-
 import Iphone11 from "./components/cases/iphone/Iphone11";
-// import Iphone11Pro from "./components/cases/iphone/Iphone11Pro";
-// import Iphone11ProMax from "./components/cases/iphone/Iphone11ProMax";
-// import Iphone12Mini from "./components/cases/iphone/Iphone12Mini";
-// import Iphone12 from "./components/cases/iphone/Iphone12";
-// import Iphone12ProMax from "./components/cases/iphone/Iphone12ProMax";
-// import Iphone13Mini from "./components/cases/iphone/Iphone13Mini";
-// import Iphone13 from "./components/cases/iphone/Iphone13";
-// import Iphone13ProMax from "./components/cases/iphone/Iphone13ProMax";
-// import Iphone14 from "./components/cases/iphone/Iphone14";
-// import Iphone14Plus from "./components/cases/iphone/Iphone14Plus";
-// import Iphone14Pro from "./components/cases/iphone/Iphone14Pro";
-// import Iphone14ProMax from "./components/cases/iphone/Iphone14ProMax";
+import BusinessCard from "./components/BusinessCard";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
         <Routes>
-          {/* <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          ></Route>
-          <Route
-            path="/update-profile"
-            element={
-              <PrivateRoute>
-                <UpdateProfile />
-              </PrivateRoute>
-            }
-          ></Route> */}
-          {/* <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} /> */}
-          <Route path="/" element={<Iphone11 />} />
-          {/* <Route path="/iphone11pro" element={<Iphone11Pro />} />
-          <Route path="/iphone11promax" element={<Iphone11ProMax />} />
-          <Route path="/iphone12mini" element={<Iphone12Mini />} />
-          <Route path="/iphone12" element={<Iphone12 />} />
-          <Route path="/iphone12promax" element={<Iphone12ProMax />} />
-          <Route path="/iphone13mini" element={<Iphone13Mini />} />
-          <Route path="/iphone13" element={<Iphone13 />} />
-          <Route path="/iphone13" element={<Iphone13 />} />
-          <Route path="/iphone13promax" element={<Iphone13ProMax />} />
-          <Route path="/iphone14" element={<Iphone14 />} />
-          <Route path="/iphone14plus" element={<Iphone14Plus />} />
-          <Route path="/iphone14pro" element={<Iphone14Pro />} />
-          <Route path="/iphone14promax" element={<Iphone14ProMax />} /> */}
+          <Route path="/" element={<BusinessCard />} />
+          <Route path="/iphone" element={<Iphone11 />} />
         </Routes>
       </AuthProvider>
     </div>
---
+++ src/components/BusinessCard.js
@@ -0,0 +1,78 @@
+import React from "react";
+import guarentee from "../images/guarentee.jpeg";
+import { useNavigate } from "react-router-dom";
+function BusinessCard() {
+    const navigate = useNavigate()
+    function handleSeeCasesClick() {
+        navigate('/iphone')
+    }
+  return (
+    <div className="business-card-full-div">
+      <div className="business-card-container">
+        <p className="shatter-header-p">Shatter & Case</p>
+        <div className="insideBusiness">
+          <div className="buy-hours-div">
+            <p className="buy-fix-p">
+              Buy - Fix - Sell - Unlock Phones & Tablets
+            </p>
+            <p className="mall-p">Holyoke Mall Hours Of Operation</p>
+            <div>
+              <p className="customer-service-p">Customer Service</p>
+              <p
+                className="number-p"
+                onClick={() => (window.location.href = "tel:4132734641")}
+              >
+                413-273-4641
+              </p>
+
+              <p className="available-p">Available for iMessage and FaceTime</p>
+              <p
+                className="email-p"
+                onClick={() =>
+                  (window.location.href = "mailto:admin@shatterandcase.com")
+                }
+              >
+                admin@shatterandcase.com
+              </p>
+
+              <div className="logo-div">
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+                <a
+                  href="https://www.facebook.com/ShatterHolyoke/"
+                  className="fa fa-facebook"
+                  aria-label="Visit our Facebook page"
+                ></a>{" "}
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+                <a
+                  href="https://www.instagram.com/shatter.case/?hl=en"
+                  className="fa fa-instagram"
+                  aria-label="Visit our Instagram page"
+                ></a>{" "}
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+                <a
+                  href="https://www.google.com"
+                  className="fa fa-snapchat-ghost"
+                  aria-label="Visit our Snapchat account"
+                ></a>{" "}
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+              </div>
+              <p className="prof-tech-p">Professional Electronics Technician</p>
+            </div>
+          </div>
+          <div className="guarentee-div">
+            <img className="guarentee-img" src={guarentee} alt="guarentee" />
+            <p>Cracked Screens</p>
+            <p>Liquid Damage</p>
+            <p>& More</p>
+            <p className="certified-p">
+              Certified Technicians Around The Clock
+            </p>
+          </div>
+        </div>
+      </div>
+      <button onClick={handleSeeCasesClick} className="cases-button">Click To See Our Cases</button>
+    </div>
+  );
+}
+
+export default BusinessCard;
